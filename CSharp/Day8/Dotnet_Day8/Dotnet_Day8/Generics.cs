3b8c4219683e1baa77598e0f2872f76e2e713a05
BanurekhaV <rekhasairam@hotmail.com>
2024-03-04 13:14:45 +0530

    day8 generics

---
+++ CSharp/Day8/Dotnet_Day8/Dotnet_Day8/Generics.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Dotnet_Day8
+{
+    class Generics
+    {
+        static void Main(string[] args)
+        {
+            List<int> numlist = new List<int>();
+            numlist.Add(45);
+            numlist.Add(56);
+            numlist.Add(10);
+            numlist.Add(6);
+
+            numlist.Sort();
+            foreach(int x in numlist)
+            {
+                Console.WriteLine(x);
+            }
+            List<string> strlist = new List<string>();
+            strlist.Add("red");
+            strlist.Add("blue");
+            strlist.Add("green");
+
+            foreach(string s in strlist)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("List with Objects-----");
+            List<Employee> emplist = new List<Employee>();
+
+            emplist.Add(new Employee(101, "Lavanya", 34500f, Convert.ToDateTime("4/01/2024")));
+            emplist.Add(new Employee(104, "Pragya", 34500f, Convert.ToDateTime("12/01/2024")));
+            emplist.Add(new Employee(102, "Khushi", 34500f, Convert.ToDateTime("27/12/2023")));
+            
+            foreach(Employee e in emplist)
+            {
+                Console.WriteLine(e.ToString());                
+            }
+            Console.Read();
+        }
+    }
+
+    //generics with user defined datatype
+
+    class Employee
+    {
+        int EID;
+        string EmpName;
+        float Salary;
+        DateTime DOJ;
+
+        public Employee(int id, string name, float sal, DateTime doj)
+        {
+            EID = id;
+            EmpName = name;
+            Salary = sal;
+            DOJ = doj;
+        }
+
+        //override tostring();
+        public override string ToString()
+        {
+            return string.Format("Employee Id : " + EID + " Employee name : " + EmpName + " Salary : " + Salary + " Date of Join : " + DOJ);
+        }
+    }
+}
---
+++ CSharp/Day8/Dotnet_Day8/Dotnet_Day8/Generics_1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Day8
+{
+    //generic class 
+
+    class GenericClass<T>
+    {
+        //generic  data member
+        T data;
+        string Gname;
+        //generic property
+        public T _Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public string _Gname
+        {
+            get { return Gname; }
+            set { Gname = value; }
+        }
+
+        //generic method
+        public void Display<T>(string msg, T arg)
+        {
+            Console.WriteLine("{0} : {1}", msg, arg);
+        }
+    }
+    class Generics_1
+    {
+        public static void Main()
+        {
+            GenericClass<int> genc = new GenericClass<int>();
+            genc._Data = 111;
+            genc._Gname = "Infinite";
+            Console.WriteLine(genc._Data + " "+ genc._Gname);
+            genc.Display<string>("It is a numbered string", "it is 5");
+            Console.WriteLine("==============");
+
+            GenericClass<string> gencstr = new GenericClass<string>();
+            gencstr._Data = "66";
+            Console.WriteLine(gencstr._Data);
+            gencstr.Display<float>("It is a Float", 34.5f);
+            Console.Read();
+
+
+        }
+    }
+}
