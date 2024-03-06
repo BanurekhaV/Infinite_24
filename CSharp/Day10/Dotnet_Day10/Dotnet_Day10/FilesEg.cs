36b8ec12e83a8bd21c776bd4ae12c585e24a27d6
BanurekhaV <rekhasairam@hotmail.com>
2024-03-06 18:26:41 +0530

    directories n files

--- CSharp/Day10/Dotnet_Day10/Dotnet_Day10/FilesEg.cs
+++ CSharp/Day10/Dotnet_Day10/Dotnet_Day10/FilesEg.cs
@@ -48,14 +48,74 @@ namespace Dotnet_Day10
 
         public static void ReadData()
         {
+            FileStream fs = new FileStream("ourfirstfile.txt", FileMode.Open,
+                FileAccess.Read);
 
+            StreamReader sr = new StreamReader(fs);
+
+            //position the file pointer at the beginning of the file
+            sr.BaseStream.Seek(20, SeekOrigin.Begin);
+
+            //read till the end of file
+            string str = sr.ReadLine();
+            while(str!=null)
+            {
+                Console.WriteLine("{0}", str);
+                str = sr.ReadLine();
+            }
+            sr.Close();
+            fs.Close();
         }
         public static void Main()
         {
             // WriteBinary();
             // ReadBinary();
-            WriteData();
+            // WriteData();
+            // ReadData();
+            DirectoriesnFiles.CheckDirectoriesnFiles();
             Console.Read();
+        }
+    }
+
+    class DirectoriesnFiles
+    {
+        public static void CheckDirectoriesnFiles()
+        {
+            //to create a new directory
+            DirectoryInfo di = new DirectoryInfo(@"C:\Banu\Infinite\B3\SampleDir");
+
+            if(di.Exists)
+            {
+                Console.WriteLine("Directory already exists..");
+            }
+            else
+            {
+                di.Create();
+                Console.WriteLine("New Directory Created..");
+            }
+
+            //this is to iterate the existing directory and get all subdirectories
+            DirectoryInfo dinfo = new DirectoryInfo(@"C:\Banu\Infinite\B3");
+            if(dinfo.Exists)
+            {
+                DirectoryInfo[] dirs = dinfo.GetDirectories();
+                foreach(object o in dirs)
+                {
+                    Console.WriteLine(o.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("The given directory does not exists");
+            }
+
+            //getting the files in the directory, their names and size using FileInfo class
+            FileInfo[] f = dinfo.GetFiles();
+
+            foreach(FileInfo fi in f)
+            {
+                Console.WriteLine($"File Name : {fi.Name}, Size is {fi.Length}");
+            }
         }
     }
 }
