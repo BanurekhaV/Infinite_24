01bb141f03a2d0930a9c96b464e37fc01278e7d2
BanurekhaV <rekhasairam@hotmail.com>
2024-03-07 11:38:21 +0530

    Serialization

---
+++ CSharp/Day11/Dotnet_Day11/Dotnet_Day11/BinareySerialization.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Dotnet_Day11
+{
+    [Serializable]  //data annotation/attribute/metadata
+    class Employee
+    {       
+        public int EID;
+        public string EName;
+        [NonSerialized]
+        public double ESal;
+
+        public Employee(int id, string name, double sal)
+        {
+            EID = id;
+            EName = name;
+            ESal = sal;
+        }
+        static void Main(string[] args)
+        {
+            Employee empobj = new Employee(1,"Sahana",35000);           
+
+            IFormatter formatter = new BinaryFormatter();
+
+            Stream stream = new FileStream(@"Employeefile.txt", FileMode.Create,
+                FileAccess.Write);
+
+            formatter.Serialize(stream, empobj);
+            stream.Close();
+
+            //Deserializing 
+
+            stream = new FileStream(@"Employeefile.txt", FileMode.Open,
+                FileAccess.Read);
+
+            Employee empdobj = (Employee)formatter.Deserialize(stream);
+
+            Console.WriteLine(empdobj.EID + " "+empdobj.EName+ " "+empdobj.ESal);
+
+            Console.Read();
+        }
+    }
+
+}
+    
---
+++ CSharp/Day11/Dotnet_Day11/Dotnet_Day11/XmlSerialization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Dotnet_Day11
+{
+    [Serializable]
+    public class Books
+    {
+        public string authorName;
+        public string bookName;
+        public int yearPublished;
+
+        public Books() { }
+        public Books(string aname,string bname, int yp)
+        {
+            this.authorName = aname;
+            this.bookName = bname;
+            this.yearPublished = yp;
+        }
+    }
+    class XmlSerialization
+    {
+        static void Main()
+        {
+            Books book = new Books("Amish Tripathi", "Seetha- the warrior of Mithila", 2010);
+
+            //serialize to an xml document
+
+            XmlSerializer xmlszr = new XmlSerializer(typeof(Books));
+
+            StreamWriter writer = new StreamWriter("Books.xml");
+
+            xmlszr.Serialize(writer.BaseStream, book);
+            writer.Close();
+
+            //deserializing
+            Books b1 = new Books();
+            XmlSerializer xl = new XmlSerializer(typeof(Books));
+            StreamReader reader = new StreamReader("Books.xml");
+            b1= (Books)xl.Deserialize(reader.BaseStream);
+            reader.Close();
+            Console.WriteLine($"{b1.authorName}, {b1.bookName}, {b1.yearPublished}");
+            Console.Read();
+        }
+    }
+}
