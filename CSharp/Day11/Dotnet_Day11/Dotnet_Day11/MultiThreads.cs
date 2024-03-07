eb57221d07f59b8da11a327f5947e5c1328b68f4
BanurekhaV <rekhasairam@hotmail.com>
2024-03-07 13:25:55 +0530

    Threads -1

---
+++ CSharp/Day11/Dotnet_Day11/Dotnet_Day11/MultiThreads.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Dotnet_Day11
+{
+    class MultiThreads
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main thread started...");
+
+            //let us create thread objects
+            Thread t1 = new Thread(Method1)
+            {
+                Name = "Thread1"
+            };
+            Thread t2 = new Thread(Method2)
+            {
+                Name = "Thread2"
+            };
+            Thread t3 = new Thread(Method3)
+            {
+                Name = "Thread3"
+            };
+                         
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            //the above association of the thread to the method name can also be 
+            //achieved as below
+            Thread t0 = new Thread(Greetings);
+            Thread t = new Thread(new ThreadStart(Greetings));
+            Thread tt = new Thread(delegate () { Greetings(); });
+            Thread ttt = new Thread(() => { Greetings(); });
+            Thread tttt = new Thread(() => Greetings());
+
+            Console.Read();
+        }
+        static void Method1()
+        {
+            Console.WriteLine("Method 1 started " + " "+ Thread.CurrentThread.Name);
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 1 : no.of times : " + i);
+            }
+        }
+        static void Method2()
+        {
+            Console.WriteLine("Method 2 started " + " " + Thread.CurrentThread.Name);
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 2 : no.of times :{0} ", i);
+                if (i == 3)
+                {
+                    Console.WriteLine("Large Operation started....");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Large operation complete...");
+                }
+            }
+        }
+        static void Method3()
+        {
+            Console.WriteLine("Method 3 started " + " " + Thread.CurrentThread.Name);
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 3 : no.of times : " + i);
+            }
+        }
+
+        static void Greetings()
+        {
+            Console.WriteLine("Hello Thread..");
+        }
+    }
+}
---
+++ CSharp/Day11/Dotnet_Day11/Dotnet_Day11/Threads_1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using System.Threading;
+
+namespace Dotnet_Day11
+{
+    class Threads_1
+    {
+        static void Main()
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+            Console.WriteLine("Current Executing Thread :" + t.Name);
+            Console.WriteLine("Current Executing Thread :" + Thread.CurrentThread.Name);
+
+            Method1();
+            Method2();
+            Method3();
+            
+            Console.Read();
+        }
+
+        static void Method1()
+        {
+            for(int i=0; i<=5; i++)
+            {
+                Console.WriteLine("Method 1 : no.of times : "+ i);
+            }
+        }
+        static void Method2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 2 : no.of times :{0} ",  i);
+                if(i==3)
+                {
+                    Console.WriteLine("Large Operation started....");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Large operation complete...");
+                }
+            }
+        }
+        static void Method3()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 3 : no.of times : " +i);
+            }
+        }
+    }
+}
