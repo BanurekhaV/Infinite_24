d0955e967a88e0f4e66b2e1c8025179c8e01b118
BanurekhaV <rekhasairam@hotmail.com>
2024-03-13 13:30:21 +0530

    Testing

---
+++ CSharp/Testing/NunitTesting/NunitTesting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTesting
+{
+    public class EmployeeDetails
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+    public class Program
+    {
+        public string Login(string UserId, string Password)
+        {
+            if(string.IsNullOrEmpty(UserId) || string.IsNullOrEmpty(Password))
+            {
+                return "UserId or Password Cannot be blank";
+            }
+            else
+            {
+                if(UserId=="Admin" && Password=="Admin@123")
+                {
+                    return "Welcome Admin";
+                }
+                return "Incorrect UserId or Password";
+            }
+        }
+
+        public List<EmployeeDetails> AllUsers()
+        {
+            List<EmployeeDetails> empdetails = new List<EmployeeDetails>()
+            {
+                new EmployeeDetails { ID = 100, Name = "Avinash", Salary = 26000 },
+                new EmployeeDetails { ID = 101, Name = "Yuvraj", Salary = 26000 },
+                new EmployeeDetails { ID = 102, Name = "Aakash", Salary = 26000 },
+                new EmployeeDetails { ID = 103, Name = "Sahana", Salary = 26000 },
+            };
+            return empdetails;
+        }
+
+        //public List<EmployeeDetails> GetEmployee(int id)
+        //{
+        //    List<EmployeeDetails> elist = new List<EmployeeDetails>();
+        //}
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CSharp/Testing/NunitTesting/TestCases/TestClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NunitTesting;
+
+namespace TestCases
+{
+   [TestFixture]
+    public class TestClass
+    {
+        //Arrange
+        Program pobj = new Program();
+
+        [Test]
+        public void TestTheLoginCredentials()
+        {
+            //Act
+           string s1 = pobj.Login("Nidhi", "N111");
+            string s2 = pobj.Login("", "");
+            string s3 = pobj.Login("Admin", "Admin@123");
+
+            //Assert
+            Assert.Equals("Incorrect UserId or Password", s1);
+            Assert.Equals("UserId or Password Cannot be blank", s2);
+            Assert.Equals("Welcome Admin", s3);
+
+        }
+        [Test]
+        public void NoTest()
+        {
+
+        }
+
+    }
+}
