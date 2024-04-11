446588fb76c051ef7342bd39df71af1dbb090fed
BanurekhaV <rekhasairam@hotmail.com>
2024-04-11 18:32:27 +0530

    custom validation

---
+++ ASP/ValidationsPrj/ValidationsPrj/CustomForm.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CustomForm.aspx.cs" Inherits="ValidationsPrj.CustomForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script type="text/javascript" lang="javascript">
+        function IsEven(source, args) {
+
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Please Enter a Positive Even Number &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtnum" runat="server"></asp:TextBox>
+            
+        &nbsp;
+            <asp:CustomValidator ID="CustomValidator1" runat="server" ControlToValidate="txtnum" 
+            ErrorMessage="Not a Positive or Even Number" ForeColor="Red" OnServerValidate="CustomValidator1_ServerValidate"
+            ValidateEmptyText="True" ClientValidationFunction="IsEven"></asp:CustomValidator>
+            <br />
+            <br />
+            <asp:Button ID="Btnsave" runat="server" Text="Save" OnClick="Btnsave_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lbl1" runat="server" Text="Label"></asp:Label>
+            
+            <br />
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/ValidationsPrj/ValidationsPrj/CustomForm.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ValidationsPrj
+{
+    public partial class CustomForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            if(args.Value =="")
+            {
+                args.IsValid = false;
+            }
+            else
+            {
+                if ((Convert.ToInt32(args.Value) > 0) && (Convert.ToInt32(args.Value) % 2 == 0))
+                {
+                    args.IsValid = true;
+                }
+                else
+                    args.IsValid = false;
+            }
+
+        }
+
+        protected void Btnsave_Click(object sender, EventArgs e)
+        {
+            if(Page.IsValid)
+            {
+                lbl1.Text = "Data Validated.., and Saving ";
+                lbl1.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lbl1.Text = "Data Validation failed.., Not Saving ";
+                lbl1.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
