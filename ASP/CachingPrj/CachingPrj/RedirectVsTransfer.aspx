5397f24de38a73942bfbeffe90b3f910f9f233fd
BanurekhaV <rekhasairam@hotmail.com>
2024-04-15 19:17:35 +0530

    Asp assignment

---
+++ ASP/CachingPrj/CachingPrj/RedirectVsTransfer.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RedirectVsTransfer.aspx.cs" Inherits="CachingPrj.RedirectVsTransfer" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtemail" runat="server"></asp:TextBox> <br />
+            <asp:Button ID="btngetdata"  runat="server" OnClick="btngetdata_Click" Text="GetPage" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/CachingPrj/CachingPrj/RedirectVsTransfer.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CachingPrj
+{
+    public partial class RedirectVsTransfer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btngetdata_Click(object sender, EventArgs e)
+        {
+            Context.Items.Add("Name", txtname.Text);
+            Context.Items.Add("Email", txtemail.Text);
+           // Server.Transfer("OtherPage.aspx");
+            Response.Redirect("OtherPage.aspx");
+        }
+    }
+}
