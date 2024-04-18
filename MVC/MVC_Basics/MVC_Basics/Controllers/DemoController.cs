df05d2a0aeb0478f93cb4d8b134b5cc3278d0b9f
BanurekhaV <rekhasairam@hotmail.com>
2024-04-18 11:05:15 +0530

    mvc 1

---
+++ MVC/MVC_Basics/MVC_Basics/Controllers/DemoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Basics.Models;
+
+namespace MVC_Basics.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+         //1. Normal Method
+         public string NormalMethod()
+        {
+            return "Hi All !!";
+        }
+
+        //2. Actionmethod
+        public ActionResult FirstMethod()
+        {
+            return View();
+        }
+
+        //3. ViewResult
+
+        public ViewResult SecondMethod()
+        {
+            return View();
+        }
+
+        //4. display either html or plain text
+        public ContentResult Reply()
+        {
+            // return Content("Good Afternoon All", "text/plain");
+            return Content("<h1 style=color:blue;>Good AfterNoon All</h1>", "text/html");
+        }
+
+        //5. Empty Result
+        [NonAction]
+        public EmptyResult CalcResult()
+        {
+            int amount = 45000;
+            float Si = (amount * 3 * 2) / 100;
+            return new EmptyResult();
+        }
+
+        //6. Json Result
+        public JsonResult EmpData()
+        {
+            Employee emp = new Employee() { ID = 101, Name = "Raja", Age = 31 };
+            return Json(emp,JsonRequestBehavior.AllowGet);
+        }
+
+        //7.RedirectResult
+        //redirects to an action method of either the same controller or different controller
+        public RedirectResult Index()
+        {
+            return Redirect("Home/Index");
+        }
+
+        //8. redirecttoaction
+
+        public ActionResult RedirectToactionmethod()
+        {
+            //  return RedirectToAction("Empdata"); //action method of the same controller
+            return RedirectToAction("Contact","Home");
+        }
+    }
+}
---
+++ MVC/MVC_Basics/MVC_Basics/Controllers/HRController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Basics.Models;
+
+namespace MVC_Basics.Controllers
+{
+    public class HRController : Controller
+    {
+        
+        //1. passing an employee object and binding it to the view
+        public ActionResult DisplayEmployee()
+        {
+            Employee emp = new Employee() { ID = 1, Name = "Aasha", Age = 26 };
+           
+            return View(emp);
+        }
+
+        //2. passing list of employees to the view
+        public ActionResult DisplayAllEmployees()
+        {
+            List<Employee> emplist = new List<Employee>()
+            {
+                new Employee(){ID=10, Name="Suryansh",Age=23},
+                new Employee(){ID=20, Name="Kavya",Age=22},
+                new Employee(){ID=30, Name="Shantanu",Age=23},
+                new Employee(){ID=40, Name="Divya",Age=22},
+            };
+            return View(emplist);
+        }
+
+        //3. Calling a view of the same controller by passing a model object /model collection
+        // GET: HR
+        public ActionResult Index()
+        {
+            List<Department> d = new List<Department>();
+            d.Add(new Department { DeptId = 1, Deptname = "IT" });
+            d.Add(new Department { DeptId = 2, Deptname = "Admin" });
+            d.Add(new Department { DeptId = 3, Deptname = "HR" });
+            d.Add(new Department { DeptId = 4, Deptname = "Sales" });
+            return View("DepartmentList",d);
+        }
+
+        //4. the called view with a list of department collection
+       // public ActionResult DepartmentList(List<Department> dlist) or
+       //with a depratment object
+        public ActionResult DepartmentList(Department dlist)
+        {
+            return View(dlist);
+        }
+
+        //5. Calling view of another controller
+        //contact of Home controller
+        public ActionResult CallContact()
+        {
+            return View("~/Views/Home/Contact.cshtml");
+        }
+    }
+}
