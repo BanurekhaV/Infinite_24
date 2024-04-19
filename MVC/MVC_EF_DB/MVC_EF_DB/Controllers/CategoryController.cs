23c4a32cf7ba28de320aa84ae2f0075e78f53b23
BanurekhaV <rekhasairam@hotmail.com>
2024-04-19 10:52:10 +0530

    ef mvc

--- MVC/MVC_EF_DB/MVC_EF_DB/Controllers/CategoryController.cs
+++ MVC/MVC_EF_DB/MVC_EF_DB/Controllers/CategoryController.cs
@@ -41,7 +41,7 @@ namespace MVC_EF_DB.Controllers
             return View();
         }
 
-        //1. pass data from view to controller using model object
+        // pass data from view to controller using model object
 
         [HttpPost]
         public ActionResult Create(Category c)
@@ -49,6 +49,63 @@ namespace MVC_EF_DB.Controllers
             db.Categories.Add(c);
             db.SaveChanges();
             return RedirectToAction("GetCategoryScafolded");
+        }
+
+        //4. delete a category
+
+        public ActionResult Delete(int id)
+        {
+            Category cat = db.Categories.Find(id);
+            return View(cat);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteCategory(int id)
+        {
+            Category c = db.Categories.Find(id);
+            db.Categories.Remove(c);
+            db.SaveChanges();
+            return RedirectToAction("GetCategoryScafolded");
+        }
+
+        //5. edit a category
+        public ActionResult Update(int id)
+        {
+            Category cat = db.Categories.Find(id);
+            return View(cat);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category c)
+        {
+            Category cat = db.Categories.Find(c.CategoryID);
+            cat.CategoryName = c.CategoryName;
+            cat.Description = c.Description;
+            cat.Picture = c.Picture;
+            db.SaveChanges();
+            return RedirectToAction("GetCategoryScafolded");
+        }
+
+        //6. details of a given category
+        public ActionResult Details(int id)
+        {
+            Category c = db.Categories.Find(id);
+            return View(c);
+        }
+
+        //7. get category by name which is sorted
+        public ActionResult GetCategoryByName()
+        {
+            List<string> sortedcat = (from cat in db.Categories
+                                      orderby cat.CategoryName
+                                      select cat.CategoryName).ToList();
+
+            //return View(sortedcat);
+            dynamic c1 = db.Categories.OrderBy(cat => cat.CategoryName).ToList();
+            //return View(c1);  // output not as expected
+            List<string> scat = c1 as List<string>;
+
+            return View(scat);
         }
     }
 }
