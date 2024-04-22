40d8394b3293546d0778c3d36f2ffeacbf84c905
BanurekhaV <rekhasairam@hotmail.com>
2024-04-22 16:28:37 +0530

    custom validations

---
+++ MVC/CustomModelValidations/CustomModelValidations/CustomValidations/ValidBirthDate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using CustomModelValidations.Models;
+
+namespace CustomModelValidations.CustomValidations
+{
+    //custom class for DOB validation
+    public sealed class ValidBirthDate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime entered_dob = Convert.ToDateTime(value);
+            DateTime mindt = Convert.ToDateTime("01/01/1995");
+            DateTime maxdt = Convert.ToDateTime("25/01/2002");
+
+            if(entered_dob >= mindt && entered_dob <= maxdt )          
+                return ValidationResult.Success;
+                else
+                    return new ValidationResult(ErrorMessage);            
+        }
+    }
+
+    public class GenderValidate : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (Convert.ToString(value) == "M" || Convert.ToString(value) == "F")
+                return ValidationResult.Success;
+            else
+                return new ValidationResult(ErrorMessage);
+        }
+    }
+}
---
+++ MVC/CustomModelValidations/CustomModelValidations/Models/JobApplication.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using CustomModelValidations.CustomValidations;
+
+namespace CustomModelValidations.Models
+{
+    public class JobApplication
+    {
+        [Required]
+        [Display(Name ="Applicant Name")]
+        public string name { get; set; }
+        [Display(Name ="Years Of Experience")]
+        [Range(3,10,ErrorMessage ="Experience must be between 3 and 10 Years")]
+        public int experience { get; set; }
+        [Display(Name ="DOB")]
+        [DataType(DataType.Date)]
+        [ValidBirthDate(ErrorMessage ="DOB should be between 01/01/1995 and 25/01/2002 only")]
+        public DateTime birthdate { get; set; }
+        [Required]
+        [Display(Name ="Email ID")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$",
+            ErrorMessage ="Invalid Email")]
+        public string email { get; set; }
+        [GenderValidate(ErrorMessage ="Please Select Your Correct Gender")]
+        public string Gender { get; set; }
+        [RegularExpression(@"^(0(?!\.00)|[1-9]\d{0,6})\.\d{2}$",
+            ErrorMessage ="Salary Should be like 6000.45")]
+        [Display(Name ="Expected Salary")]
+        public decimal expsal { get; set; }
+        [SkillValidation(ErrorMessage ="Select atleast 3 skills")]
+        public List<CheckBox> Skills { get; set; }
+        [Required]
+        [Display(Name ="Do You Have Passport ?")]
+        public string HavePassport { get; set; }
+    }
+}
